08e6d085c4460a6e7b90774ad98ef1cac56b62e4
André Pereira da Silva <andre.pereira@matera.com>
2025-04-03 02:10:10 -0300

    Initial commit

---
+++ src/main/java/com/algaworks/algatransit/AlgaTransitApiApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.algatransit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaTransitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaTransitApiApplication.class, args);
+	}
+}
