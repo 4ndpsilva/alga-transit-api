d780981ced0ac2231c0d9601bbe6f99a8ea3b254
André Pereira da Silva <andre.pereira@matera.com>
2025-04-14 04:07:39 -0300

    Added endpoints for queries

--- src/main/java/com/algaworks/algatransit/application/controller/OwnerController.java
+++ src/main/java/com/algaworks/algatransit/application/controller/OwnerController.java
@@ -2,36 +2,41 @@ package com.algaworks.algatransit.application.controller;
 
 import com.algaworks.algatransit.domain.model.entity.Owner;
 import com.algaworks.algatransit.domain.repository.OwnerRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+
 @RestController
 @RequestMapping("/api/v1/owners")
+@RequiredArgsConstructor
 public class OwnerController {
-
-    @PersistenceContext
-    private EntityManager entityManager;
 
     private final OwnerRepository repository;
 
     @GetMapping
     public ResponseEntity<List<Owner>> find(){
-        /*String jpql = "from Owner";
-        TypedQuery<Owner> q = entityManager.createQuery(jpql, Owner.class);
-        return ResponseEntity.ok(q.getResultList());*/
-
         return ResponseEntity.ok(repository.findAll());
     }
 
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<Owner>> findById(@PathVariable String name){
+        return ResponseEntity.ok(repository.findByNameContaining(name));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Owner> findById(@PathVariable Long id){
+        Optional<Owner> opOwner = repository.findById(id);
+        return opOwner.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
     public ResponseEntity<Owner> save(@RequestBody Owner owner){
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(repository.save(owner));
     }
 }
--- src/main/java/com/algaworks/algatransit/domain/repository/OwnerRepository.java
+++ src/main/java/com/algaworks/algatransit/domain/repository/OwnerRepository.java
@@ -1,6 +1,9 @@
 package com.algaworks.algatransit.domain.repository;
 
 import com.algaworks.algatransit.domain.model.entity.Owner;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OwnerRepository extends JpaRepository<Owner, Long> { }
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+    List<Owner> findByNameContaining(String name);
+}
